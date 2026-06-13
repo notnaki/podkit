646c1963d3e71ddbdc6c0aaeb5021d0b74baa193
Nuh Naci Kusculu <jr.nuhnaci@gmail.com>
2026-06-13 23:27:00 +0300

    fix(framework): type page component as ReactNode for clean typecheck

--- packages/framework/src/render/ssr.ts
+++ packages/framework/src/render/ssr.ts
@@ -1,14 +1,16 @@
-import { createElement } from "react";
+import { createElement, type ReactNode } from "react";
 import { renderToString } from "react-dom/server";
 import { htmlDocument } from "./document.ts";
 import type { RouteModule } from "../loader/run.ts";
+
+type PageComponent = (props: { data: unknown }) => ReactNode;
 
 export async function renderPage(
   mod: RouteModule,
   data: unknown,
   clientEntry: string,
 ): Promise<string> {
-  const Component = mod.default as ((props: { data: unknown }) => unknown) | undefined;
-  const appHtml = Component ? renderToString(createElement(Component, { data }) as never) : "";
+  const Component = mod.default as PageComponent | undefined;
+  const appHtml = Component ? renderToString(createElement(Component, { data })) : "";
   return htmlDocument(appHtml, data, clientEntry);
 }
